935d0f91c3758c436403e4d814125c7d6d926d08
Diego Borja <diarborj@bancolombia.com.co>
2025-06-18 20:12:22 -0500

    cambio

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/api/mock', (req, res) => {
-  res.json({ message: 'service 2 running cambio', timestamp: new Date() });
+  res.json({ message: 'service 2 running cambio 2', timestamp: new Date() });
 });
 
 app.listen(PORT, () => {
